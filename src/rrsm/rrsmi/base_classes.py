513e1693504886b02facf5fad7da0ef064bbb949
Jarosław Bieńkowski <jaroslaw.bienkowski@hotmail.com>
2018-04-05 09:23:04 +0000

    Adde some code to handle logging

---
+++ src/rrsm/rrsmi/base_classes.py
@@ -0,0 +1,8 @@
+from django.views.generic import UpdateView
+from django.utils import timezone
+from django.http import Http404
+
+from .logger import RrsmLoggerMixin
+
+class RrsmHelpers(RrsmLoggerMixin):
+    pass
