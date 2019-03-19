7b1c8d03a584e7b551d24e6635a82537b3f7b73f
Jarek Bienkowski <jarek@macjarek.knmi.nl>
2019-03-19 15:44:19 +0100

    WIP * Updated packages * Use port 80 to get waveforms

--- src/rrsm/rrsm/settings.py
+++ src/rrsm/rrsm/settings.py
@@ -203,7 +203,7 @@ URL_MOTION = 'http://www.orfeus-eu.org/odcws/rrsm/1/peak-motion'
 URL_SHAKEMAP = 'http://www.orfeus-eu.org/odcws/rrsm/1/shakemap'
 URL_WAVEFORM = 'http://www.orfeus-eu.org/odcws/rrsm/1/waveform'
 URL_ROUTING = 'http://www.orfeus-eu.org/eidaws/routing/1/query'
-URL_ODC_API = 'https://orfeus-eu.org/api/waveform'
+URL_ODC_API = 'http://orfeus-eu.org/api/waveform'
 
 # Cache
 CACHE_TIME_SHORT = config('CACHE_TIME_SHORT')
